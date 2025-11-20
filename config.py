e1f9356a628cc0a82ec0c0bd2a2e161c8151180f
Getepq <114354569+Getepq@users.noreply.github.com>
2025-11-21 00:06:24 +0500

    Update config.py

--- config.py
+++ config.py
@@ -6,6 +6,16 @@ load_dotenv()
 DISCORD_TOKEN = os.getenv('DISCORD_TOKEN')
 PERPLEXITY_API_KEY = os.getenv('PERPLEXITY_API_KEY')
 
+# Исправление для GUILD_ID
+guild_id_env = os.getenv('GUILD_ID')
+if guild_id_env:
+    try:
+        ALLOWED_GUILD_ID = int(guild_id_env)
+    except (ValueError, TypeError):
+        ALLOWED_GUILD_ID = None
+else:
+    ALLOWED_GUILD_ID = None
+
 BOT_PREFIX = '!'
 EMBED_COLOR = 0x5865F2
 
